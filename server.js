b4f11c2d3e8dcc37c58c6b0e549cea572714f4be
Jonathan Medina <jonathanmedhed@gmail.com>
2020-12-22 08:01:08 -0400

    fix server.js v2

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 
+const __dirname = path.resolve()
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/frontend/build')))
 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
